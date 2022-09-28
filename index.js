95041b11aebe0c0d5d175ad282648a60e7d86f75
Olutola Oloruntobi Paul <oloruntobiolutola@gmail.com>
2022-09-28 15:48:28 +0100

    user-model

--- index.js
+++ index.js
@@ -4,18 +4,4 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/signup", async (req, res) => {
-  try {
-    console.log(req.body);
-    const user = await User.create(req.body);
-    res.status(201).json({
-      user,
-    });
-  } catch (error) {
-    res.status(400).json({
-      message: error,
-    });
-  }
-});
-
 module.exports = app;
