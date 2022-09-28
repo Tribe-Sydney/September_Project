76b37e32a97d642a66915d3ba2370ed0145053bf
Okoli kingsley <kingxleeya@gmail.com>
2022-09-28 19:35:32 +0100

    New updates

---
+++ routes/user-routes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+
+const {
+    signUp,
+    signIn,
+    deleteUser,
+    updateUser,
+    getAll,
+    getOne,
+} = require('../controller/new')
+
+router.post("/signup", signUp);
+router.post("/signin", signIn);
+router.route('/').get(getAll).post(createUser)
+router.route("/:id").delete(deleteUser).patch(updateUser).get(getOne);
+module.export = router
