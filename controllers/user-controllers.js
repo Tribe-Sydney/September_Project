8211693dace5fcd4596e8da341ed2a458525fb56
Olutola Oloruntobi Paul <oloruntobiolutola@gmail.com>
2022-09-29 15:35:14 +0100

    refactoring

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,91 +1,69 @@
 const User = require("../models/User.js");
 const CatchAsync = require("../utils/catch-async.js");
+const ErrorObject = require("../utils/error.js");
 
-//User signup handler
-
-exports.deleteUser = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return res.status(400).json({
-        status: "fail",
-        message: `There is no user with the id ${req.params.id}`,
-      });
-    }
-    await User.findByIdAndDelete(req.params.id);
-    res.status(204).json({
-      status: "successful deleted",
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err,
-    });
+// Delete A User
+exports.deleteUser = CatchAsync(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  if (!user) {
+    return next(
+      new ErrorObject(`There is no user with the id ${req.params.id}`, 400)
+    );
   }
-};
+  await User.findByIdAndDelete(req.params.id);
+  res.status(204).json({
+    status: "success",
+  });
+});
 
-exports.updateUser = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return res.status(400).json({
-        status: "fail",
-        message: `There is no user with the id ${req.params.id}`,
-      });
-    }
-    const email = req.body.email === undefined ? user.email : req.body.email;
-    const fullName =
-      req.body.fullName === undefined ? user.fullName : req.body.fullName;
-
-    const updatedUser = await User.findByIdAndUpdate(req.params.id, update, {
-      new: true,
-      runValidators: true,
-    });
-    res.status(200).json({
-      status: "success",
-      data: {
-        updatedUser,
-      },
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: "fail",
-      message: error,
-    });
+// Update A User
+exports.updateUser = CatchAsync(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  if (!user) {
+    return next(
+      new ErrorObject(`There is no user with the id ${req.params.id}`, 400)
+    );
   }
-};
+  const email = req.body.email === undefined ? user.email : req.body.email;
+  const fullName =
+    req.body.fullName === undefined ? user.fullName : req.body.fullName;
+  const update = { email, fullName };
+  const updatedUser = await User.findByIdAndUpdate(req.params.id, update, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: "success",
+    data: {
+      user: updatedUser,
+    },
+  });
+});
 
-//Get All Users
-exports.getAllUser = async (req, res) => {
-  try {
-    const user = await User.find();
-    res.status(200).json({
-      results: user.length,
-      data: {
-        user,
-      },
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: "fail",
-      message: error,
-    });
-  }
-};
+//  Get All Users
+exports.getAllUser = CatchAsync(async (req, res, next) => {
+  const users = await User.find();
+  res.status(200).json({
+    status: "success",
+    results: users.length,
+    data: {
+      users,
+    },
+  });
+});
 
-//Get One User
-exports.getOneUser = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    res.status(200).json({
-      data: {
-        user,
-      },
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: "fail",
-      message: error,
-    });
+//  Get One User
+exports.getOneUser = CatchAsync(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  if (!user) {
+    return next(
+      new ErrorObject(`There is no user with the id ${req.params.id}`, 400)
+    );
   }
-};
+  res.status(200).json({
+    status: "success",
+    data: {
+      user,
+    },
+  });
+});
